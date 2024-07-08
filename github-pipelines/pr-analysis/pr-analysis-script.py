e019ae8e94fcd3890bc1ad1e535aca7073e17802
Szczepaniak-M <m.szczepaniak99@gmail.com>
2024-07-08 12:34:54 +0200

    Fix pipeline

--- github-pipelines/pr-analysis/pr-analysis-script.py
+++ github-pipelines/pr-analysis/pr-analysis-script.py
@@ -92,7 +92,7 @@ def analyze_configuration_file(yaml_file):
             if not isinstance(yaml_data['configuration']['instance-types'], list):
                 print_stderr(f"'instance-types' in 'configuration' should be of type list.")
                 return 1
-            client = boto3.client('ec2')
+            client = boto3.client('ec2', region_name='us-east-1')
             aws_instances = client._service_model.shape_for('InstanceType').enum
             for instance_type in yaml_data['configuration']['instance-types']:
                 if instance_type not in aws_instances:
@@ -101,11 +101,11 @@ def analyze_configuration_file(yaml_file):
             instance_specified = True
 
         # Check specified tags
-        allowed_tags = get_allowed_tags()
         if 'instance-tags' in yaml_data['configuration']:
             if not isinstance(yaml_data['configuration']['instance-tags'], list):
                 print_stderr(f"'instance-tags' in 'configuration' should be of type list.")
                 return 1
+            allowed_tags = get_allowed_tags()
             for tag_list in yaml_data['configuration']['instance-tags']:
                 if not isinstance(tag_list, list):
                     print_stderr(f"All elements in 'instance-tags' should be of type list.")
@@ -235,7 +235,7 @@ def analyze_configuration_file(yaml_file):
                 if not isinstance(node['instance-type'], str):
                     print_stderr(f"'instance-type' in 'node' should be of type str.")
                     return 1
-                client = boto3.client('ec2')
+                client = boto3.client('ec2', region_name='us-east-1')
                 aws_instances = client._service_model.shape_for('InstanceType').enum
                 if node['instance-type'] not in aws_instances:
                     print_stderr(f"Incorrect value of EC2 instance in 'instance-type' for node with ID = {node['node-id']}.")
